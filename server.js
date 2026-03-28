e3544ef8eef6afff0da6936b979a7cd599628fca
Shubham Goyal <gshubham.05@gmail.com>
2026-03-28 10:42:46 +0530

    env

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from 'express'
-import userRoute from './routes/userRoute.js'
-let app = express()
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoute from "./routes/userRoute.js";
+let app = express();
 //setting view engine
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 // middleware
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(userRoute)
-
-app.listen(9000,()=>console.log("running.."))
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(userRoute);
+let port = process.env.port || 9000
+app.listen(port, () => console.log("running..", port));
