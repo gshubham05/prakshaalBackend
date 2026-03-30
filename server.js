868c0368bbcf9b5e726335315f67330d4d1ce4f9
Shubham Goyal <gshubham.05@gmail.com>
2026-03-30 11:44:02 +0530

    multer

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let app = express();
 app.set("view engine", "ejs");
 // middleware
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 app.use(userRoute);
 let port = process.env.port || 9000
 app.listen(port, () => console.log("running..", port));
