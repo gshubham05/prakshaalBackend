2d097ce30b41102edbbc65e3bbfc0afd5ba36323
Shubham Goyal <gshubham.05@gmail.com>
2026-04-06 11:39:02 +0530

    bcypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,52 @@
-export let home = (req,res)=>{
-    res.render("index")
-}
+import bcrypt from "bcrypt";
+let userData = [
+  {
+    name: "nitin",
+    pwd: "$2b$10$jVqXyvJUSSas/ldJV9hUpOBS4Jsl6IYHXaAg4nRfyHsEdi09qU6YC",
+    email: "nitin@gmail.com",
+  },
+    ];
 
-export let about = (req,res)=>{
-    res.send("about")
-}
+export let home = (req, res) => {
+  res.render("index");
+};
 
-export let dynamicID = (req,res)=>{
-    const {pid} = req.params
-    res.send(`<h1>welcome to my page mr : ${pid}</h1>`)
-   
-}
+export let about = (req, res) => {
+  res.send("about");
+};
+
+export let dynamicID = (req, res) => {
+  const { pid } = req.params;
+  res.send(`<h1>welcome to my page mr : ${pid}</h1>`);
+};
+
+export let register = (req, res) => {
+  let { name, email, pwd } = req.body;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(pwd, salt, function (err, hash) {
+      console.log("hash : ", hash);
+      userData.push({ name, pwd: hash, email });
+      res.redirect("/login");
+    });
+  });
+};
+
+export let login = (req, res) => {
+  console.log(userData);
+  res.render("login");
+};
+
+export let loginCheck = async(req, res) => {
+  const {email,pwd}= req.body;
+  let u1 = userData.find(i=>i.email ==email)
+  if(!u1){
+    return res.send("first register yourself")
+  }
+  let isMatch =await bcrypt.compare(pwd,u1.pwd)
+  if(!isMatch){
+    return res.send("invalid Credentials")
+  }
+
+  res.render("dashboard")
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,35 +1,24 @@
 import express from "express";
-import { about, dynamicID, home } from "../controllers/userController.js";
+import {
+  about,
+  dynamicID,
+  home,
+  login,
+  loginCheck,
+  register,
+} from "../controllers/userController.js";
 import upload from "../config/multer.js";
 
 let userRoute = express.Router();
 
 userRoute.get("/", home);
+userRoute.get("/login", login);
+userRoute.post("/login", loginCheck);
+userRoute.get("/register", (req, res) => {
+  res.render("index");
+});
+userRoute.post("/register", register);
 userRoute.get("/about", about);
 userRoute.get("/:pid", dynamicID);
-
-userRoute.post(
-  "/login",
-  upload.fields([
-    { name: "photo", maxCount: 4 },
-    { name: "img1", maxCount: 1 },
-  ]),
-  (req, res) => {
-    console.log("login post route ", req.body);
-    console.log(req.file);
-  },
-);
-
-// Array of images
-// userRoute.post('/login',upload.array("photo",5),(req,res)=>{
-//     console.log("login post route ",req.body)
-//     console.log(req.file)
-// })
-
-// single photo
-// userRoute.post('/login',upload.single("photo"),(req,res)=>{
-//     console.log("login post route ",req.body)
-//     console.log(req.file)
-// })
 
 export default userRoute;
